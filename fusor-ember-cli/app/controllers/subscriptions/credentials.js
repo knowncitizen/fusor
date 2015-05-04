60abf17846a704073c375888d05f4e8bb17c5371
Joseph Magen <jmagen@redhat.com>
2015-05-04 16:52:43 +0300

    squash this commit

--- fusor-ember-cli/app/controllers/subscriptions/credentials.js
+++ fusor-ember-cli/app/controllers/subscriptions/credentials.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
+import LoginControllerMixin from 'simple-auth/mixins/login-controller-mixin';
 
-export default Ember.Controller.extend({
+export default Ember.Controller.extend(LoginControllerMixin, {
 
   username: null,
   password: null,
@@ -8,6 +9,54 @@ export default Ember.Controller.extend({
 
   disableCredentialsNext: function() {
     return (Ember.isBlank(this.get('username')) || Ember.isBlank(this.get('password')));
-  }.property('username', 'password')
+  }.property('username', 'password'),
+
+  authenticator: 'authenticator:rhportal',
+
+  actions: {
+
+    authenticate: function() {
+      var self = this;
+      alert('got here');
+
+      // if (this.get('authType') == 'Basic') {
+        return this._super().then(function() {
+      //         var adapter = self.store.adapterFor('ApplicationAdapter');
+      //         adapter.set('headers', { Authorization: 'Basic ' + self.get('session.basicAuthToken') });
+               return self.transitionTo('subscriptions.management-application');
+      //     }, function () {
+      //           return self.set('errorMessage', "Your username or password is incorrect. Please try again.");
+           });
+
+      // } else {
+
+      //     return this._super().then(function() {
+
+      //         var adapter = self.store.adapterFor('ApplicationAdapter');
+      //         // add token to adapter header
+      //         adapter.set('headers', { Authorization: 'Bearer ' + self.get('session.access_token') });
+
+      //         // add user to local storage session
+      //         self.store.find('user', self.get('identification')).then(function(response) {
+      //           self.get('session').set('authType', 'oAuth');
+      //           self.get('session').set('currentUser', response);
+      //           console.log('SESSION');
+      //           console.log(self.get('session'));
+      //           console.log(self.get('session.authType'));
+      //           console.log(self.get('session.basicAuthToken'));
+      //           return self.transitionTo('deployment-new.start');
+      //         },
+      //           function(response){
+      //             alert('error oAuth')
+      //           }
+      //         );
+
+      //       }, function() {
+      //             return self.set('errorMessage', "Your username or password is incorrect. Please try again.");
+      //     });
+      }
+
+    },
+
 
 });
